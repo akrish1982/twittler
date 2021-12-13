24ddbc9f9f3e23baab33650f6cb8b8a37c916319
Ananth Tirumanur <akrish1982@gmail.com>
2021-12-13 10:53:40 -0500

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -2,11 +2,11 @@ const faunadb = require('faunadb')
 const {TwitterApi} = require('twitter-api-v2')
 
 //const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN)
-const twitterClient = new TwitterApi({
-  consumer_key: process.env.API_KEY,
-  consumer_secret: process.env.API_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+const twitterClient = new TwitterApi({  
+  appKey: process.env.API_KEY,  
+  appSecret : process.env.API_SECRET,
+  access_token : process.env.ACCESS_TOKEN,
+  access_token_secret : process.env.ACCESS_TOKEN_SECRET
 })
 const q = faunadb.query
 
@@ -30,12 +30,22 @@ module.exports = async (req, res) => {
           q.Lambda(['date', 'ref'], q.Get(q.Var('ref')))
         )
       )
-    
+      console.log('I am new code to create auth link')
+      const authLink = await twitterClient.generateAuthLink();
+
+      // By default, oauth/authenticate are used for auth links, you can change with linkMode
+      // property in second parameter to 'authorize' to use oauth/authorize
+      const authLink = await twitterClient.generateAuthLink({ linkMode: 'authorize' });
+
+      // Use URL generated
+      const authUrl = authLink.url;
+      console.log('I am new code to create auth link')
+      
       console.log('I was triggered before posting to twitter')
       // post all tweets from date range on twitter
       data.forEach(async ({data: {tweet}}) => {
         console.log('I have data')
-        await twitterClient.v1.tweet(tweet)
+        await twitterClient.v1.tweet('I have data')
         console.log('I Tweeted!!!')
       })
       res.status(200).json({success: true})
